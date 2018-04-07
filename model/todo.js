96d8cc7bafea2a0c7b02f0d0dff178d1d7863d2a
Welliton Portilho de Paiva <wellitonpaiva@gmail.com>
2018-04-07 00:27:25 -0300

    added mongoose, express and configured dbAtlas

---
+++ model/todo.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var Todo = mongoose.model('Todo', {
+  text : String
+});
---
+++ server.js
@@ -0,0 +1,69 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+/* mongoose.connect('mongodb+srv://user:' +
+  'tdy8Bch0llJrm9ao@cluster0-nbz6s.mongodb.net/test'); */
+
+mongoose.connect('mongodb://user:tdy8Bch0llJrm9ao' +
+  '@cluster0-shard-00-00-nbz6s.mongodb.net:27017,' +
+  'cluster0-shard-00-01-nbz6s.mongodb.net:27017,' +
+  'cluster0-shard-00-02-nbz6s.mongodb.net:27017/test?' +
+  'ssl=true&replicaSet=Cluster0-shard-0&authSource=admin');
+
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+app.unsubscribe(bodyParser.urlencoded({extended: 'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+app.use(methodOverride());
+
+app.listen(8080);
+
+// model
+var todo = mongoose.model('todo', {
+  text: String,
+});
+
+// routes
+app.get('/api/todos', function(req, res) {
+  todo.find(function(err, todos) {
+    if (err)
+      res.send(err);
+    res.json(todos);
+  });
+});
+
+app.post('/api/todos', function(req, res) {
+  todo.create({
+    text: req.body.text,
+    done: false,
+  }, function(err, todo) {
+    if (err)
+      res.send(err);
+    todo.find(function(err, todos) {
+      if(err)
+        res.send(err);
+      res.json(todos);
+    });
+  });
+});
+
+app.delete('/api/todos/:todo_id', function(req, res) {
+  todo.remove({
+    _id: req.params.todo_id,
+  }, function(err, todo) {
+    if (err)
+      res.send(err);
+      todo.find(function(err, todos) {
+        if(err)
+          res.send(err);
+        res.json(todos);
+    });
+  });
+});
